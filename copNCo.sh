afc805237254be1b07682c96d3683cd882757616
achamney <achamney@gmail.com>
2015-05-28 21:06:48 -0400

    Update to stuff

---
+++ copNCo.sh
@@ -0,0 +1,7 @@
+rm -r ~/Downloads/LisAndAusWedding/*
+cp -r * ~/Downloads/LisAndAusWedding/
+git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding add --all .
+git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding commit -m "Update to stuff"
+
+
+
