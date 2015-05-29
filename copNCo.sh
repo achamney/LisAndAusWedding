243f33efae1663985989dc3110ed0be8adff0a11
achamney <achamney@gmail.com>
2015-05-28 21:13:07 -0400

    Update to stuff

--- copNCo.sh
+++ copNCo.sh
@@ -2,7 +2,7 @@ rm -r ~/Downloads/LisAndAusWedding/*
 cp -r * ~/Downloads/LisAndAusWedding/
 git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding add --all .
 git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding commit -m "Update to stuff"
-git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding push
+git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding push origin master
 
 
 
--- copyNcommit.sh
+++
@@ -1,6 +0,0 @@
-rm -r ~/Downloads/LisAndAusWedding/*
-
-cp -r * ~/Downloads/LisAndAusWedding/
-
-svn add --force ~/Downloads/LisAndAusWedding/* --auto-props --parents --depth infinity -q 
-svn commit ~/Downloads/LisAndAusWedding/ 
