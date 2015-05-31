34a3cee2357da84e3b02729180e7de82f7422b15
achamney <achamney@gmail.com>
2015-05-31 11:52:46 -0400

    Added some useful scripts

--- copNCo.sh
+++ copNCo.sh
@@ -1,8 +1,6 @@
 rm -r ~/Downloads/LisAndAusWedding/*
 cp -r * ~/Downloads/LisAndAusWedding/
-git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding add .
-git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding commit -m "Update to stuff"
-git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding push origin master
+ls ~/Downloads/LisAndAusWedding/
 
 
 
---
+++ updateFromRepo.sh
@@ -0,0 +1,8 @@
+git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding pull origin master
+rm -r ~/Downloads/tomcat/webapps/Root/wedding/*
+cp -r ~/Downloads/LisAndAusWedding/* ~/Downloads/tomcat/webapps/Root/wedding/
+
+
+
+
+
