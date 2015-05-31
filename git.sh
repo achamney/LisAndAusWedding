0d428fad3e457daf705eecbf31e427de623e52e5
achamney <achamney@gmail.com>
2015-05-31 11:46:23 -0400

    Revert Last Commit

---
+++ git.sh
@@ -0,0 +1,6 @@
+git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding $@
+
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,20 @@
+var images = ['http://i.imgur.com/bGjZeQ1.jpg','http://i.imgur.com/K0Vvsfm.jpg',
+				'http://i.imgur.com/SQYMSt6.jpg',
+				'http://i.imgur.com/Nx52QkB.jpg',
+				'http://i.imgur.com/Xk2rwi2.jpg',
+				'http://i.imgur.com/kxzyIK2.jpg',
+				
+				'http://i.imgur.com/RfUbxbM.jpg',
+				'http://i.imgur.com/0cvsf0c.jpg',
+				'http://i.imgur.com/BlTXj9I.jpg',
+				'http://i.imgur.com/acB3c6K.jpg',
+				'http://i.imgur.com/t9KYced.jpg'];
+				
+var caro = $('.carousel-inner');
+images.forEach(function(img,i){
+	var item=$('<div class="item"></div>');
+	if(i==0)
+		item.addClass('active');
+	caro.append(item);
+	item.append($('<img width=800 src="'+img+'">'));
+});
