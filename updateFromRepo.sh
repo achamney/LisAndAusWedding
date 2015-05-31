933c00d50bb11b47954887f4c462be5f930623aa
achamney <achamney@gmail.com>
2015-05-31 12:08:57 -0400

    fixed update sh

--- updateFromRepo.sh
+++ updateFromRepo.sh
@@ -1,6 +1,6 @@
 git --git-dir=/home/fredsnothere/Downloads/LisAndAusWedding/.git/ --work-tree=/home/fredsnothere/Downloads/LisAndAusWedding pull origin master
-rm -r ~/Downloads/tomcat/webapps/Root/wedding/*
-cp -r ~/Downloads/LisAndAusWedding/* ~/Downloads/tomcat/webapps/Root/wedding/
+rm -r ~/Downloads/tomcat/webapps/ROOT/wedding/*
+cp -r ~/Downloads/LisAndAusWedding/* ~/Downloads/tomcat/webapps/ROOT/wedding/
 
 
 
