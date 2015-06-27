cefcdd6e1997afe7c6f6c9f0db7c2de4ada0f786
achamney <achamney@gmail.com>
2015-06-27 11:47:58 -0400

    Added backup of database

--- copNCo.sh
+++ copNCo.sh
@@ -1,7 +1,10 @@
 rm -r ~/Downloads/LisAndAusWedding/*
 cp -r * ~/Downloads/LisAndAusWedding/
+touch ~/Downloads/LisAndAusWedding/backup.zip
+mysqldump --user=blog --password=password --default-character-set=utf8 wedding | gzip > "/home/fredsnothere/Downloads/LisAndAusWedding/backup.zip"
 ls ~/Downloads/LisAndAusWedding/
-
+./git.sh add .
+./git.sh commit
 
 
 
