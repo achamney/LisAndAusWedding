68dade539fafb85dbb0f44ee1dfde0cc50106289
Farshad.Muhammad <farshadmuhammad@cmail.carleton.ca>
2015-09-20 16:53:50 -0400

    First commit

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ var images = ['http://i.imgur.com/bGjZeQ1.jpg','http://i.imgur.com/K0Vvsfm.jpg',
 				'http://i.imgur.com/RfUbxbM.jpg',
 				'http://i.imgur.com/0cvsf0c.jpg',
 				'http://i.imgur.com/BlTXj9I.jpg',
-				'http://i.imgur.com/acB3c6K.jpg',
-				'http://i.imgur.com/t9KYced.jpg'];
+				'http://i.imgur.com/acB3c6K.jpg'];
 				
 var caro = $('.carousel-inner');
 images.forEach(function(img,i){
